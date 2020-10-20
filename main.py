03a56e937ec6107ab61ff3a8ce7c979d1d26c80e
Arjun <iarjun.sahlot@gmail.com>
2020-10-20 12:02:17 -0700

    Licensing Commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import os
 from Pixelator.constants import *
 from Pixelator.classes import Image
-from pumpkinpy import pygame as pg
+from pumpkinpy.pygameutils import elements as pg
 from tkinter.filedialog import askdirectory, askopenfilename
 
 
@@ -29,10 +29,10 @@ def draw_window(win, width, height, image, open_button, pixel_button, save_butto
 def main(win, width, height):
     clock = pygame.time.Clock()
     image = Image(os.path.join("assets", "starter_image.png"), 50, 50)
-    open_button = pg.Button((1250 - 100, 100), (200, 60), FONT, "Open", GREEN, borderThickness=5)
-    pixel_button = pg.Button((1250 - 125, 200), (250, 80), B_FONT, "Pixelate", GREEN, borderThickness=5)
-    save_button = pg.Button((1250 - 100, 320), (200, 60), FONT, "Save", GREEN, borderThickness=5)
-    resolution_text = pg.TextInput((1250 - 100, 650), (200, 50), initial_string="Resolution")
+    open_button = pg.ButtonText((1250 - 100, 100), (200, 60), (0, 255, 0), (0, 200, 0), (0, 255, 0), FONT.render("Open", 1, BLACK), (0, 0), 5, BLACK)
+    pixel_button = pg.ButtonText((1250 - 125, 200), (250, 80), (0, 255, 0), (0, 200, 0), (0, 255, 0), B_FONT.render("Pixelate", 1, BLACK), (0, 0), 5, BLACK)
+    save_button = pg.ButtonText((1250 - 100, 320), (200, 60), (0, 255, 0), (0, 200, 0), (0, 255, 0), FONT.render("Save", 1, BLACK), (0, 0), 5, BLACK)
+    resolution_text = pg.TextInput((1250 - 100, 650), (200, 50), initialText="Resolution")
     typing = False
     run = True
     while run:
@@ -40,17 +40,17 @@ def main(win, width, height):
         draw_window(win, width, height, image, open_button, pixel_button, save_button)
         events = pygame.event.get()
         image.update(win, events)
-        if open_button.Clicked():
+        if open_button.clicked:
             image.change_image(askopenfilename())
-        if pixel_button.Clicked():
+        if pixel_button.clicked:
             try:
-                res = int(resolution_text.get_text())
+                res = int(resolution_text.text)
                 image.pixelate(res)
             except ValueError:
                 resolution_text.input_string = "200"
                 image.pixelate(200)
 
-        if save_button.Clicked():
+        if save_button.clicked:
             image.save(askdirectory())
         for event in events:
             if event.type == pygame.QUIT:
@@ -58,16 +58,14 @@ def main(win, width, height):
                 # exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if resolution_text.rect.collidepoint(event.pos):
-                    typing = True
-                    if resolution_text.get_text() == "Resolution":
-                        resolution_text.clear_text()
+                    if resolution_text.text == "Resolution":
+                        resolution_text.ClearText()
                 else:
-                    typing = False
-                    if resolution_text.get_text() == "":
+                    if resolution_text.text == "":
                         resolution_text.input_string = "Resolution"
 
-        resolution_text.update(events, typing)
-        resolution_text.draw(win)
+        resolution_text.Update(events)
+        resolution_text.Draw(win)
 
         pygame.display.update()
 
